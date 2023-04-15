811fc68c8738d3c04696cbc1fa610c787aa48efd
Tenzin Tsephel <108427228+Bakashi160302@users.noreply.github.com>
2023-04-15 15:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import mysql.connector
 mydb = mysql.connector.connect(
-     host="localhost",
-     user="root",
-     password=""
- )
+    host="ycc-project.cvcpbdfon0xp.us-east-1.rds.amazonaws.com",  # Replace with your RDS endpoint
+    port="3306",             # Replace with your RDS port number, typically 3306 for MySQL
+    user="root",           # Replace with your RDS username
+    password="tejal0910&",       # Replace with your RDS password
+    database="PES2UG20CS370_Train_lab10"
+)
 c = mydb.cursor()
 # c.execute("CREATE DATABASE PES2UG20CS370_Train_Lab10")
 
