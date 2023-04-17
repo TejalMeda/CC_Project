08926076bc452d4db765cfcc9652a1354357c4cc
Tenzin Tsephel <108427228+Bakashi160302@users.noreply.github.com>
2023-04-17 19:34:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+# pip install mysql-connector-python
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -10,42 +11,44 @@ mydb = mysql.connector.connect(
 c = mydb.cursor()
 
 
-
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS PES2UG20CS370_Train (Train_No int, Name varchar(30), Train_Type varchar(20), Source varchar(20), Destination varchar(20), Availability varchar(5));')
+    c.execute('CREATE TABLE IF NOT EXISTS TRAIN(train_no TEXT, train_name TEXT, train_type TEXT, '
+              'train_source TEXT,train_destination TEXT,train_availability TEXT)')
 
 
-def add_data(Train_no,Name,Train_type,source,Destination,Availability):
-    c.execute('INSERT INTO PES2UG20CS370_Train(Train_no,Name,Train_type,source,Destination,Availability) VALUES (%s,%s,%s,%s,%s,%s)',(Train_no, Name, Train_type, source, Destination, Availability))
+def add_data(train_no, train_name, train_type, train_source, train_destination,train_availability):
+    c.execute('INSERT INTO TRAIN(train_no, train_name, train_type, train_source, train_destination,train_availability) VALUES (%s,%s,%s,'
+              '%s,%s,%s)',
+              (train_no, train_name, train_type, train_source, train_destination,train_availability))
     mydb.commit()
 
 
 def view_all_data():
-    c.execute('SELECT * FROM PES2UG20CS370_Train')
+    c.execute('SELECT * FROM TRAIN')
     data = c.fetchall()
     return data
 
 
-def view_only_Train_names():
-    c.execute('SELECT Name FROM PES2UG20CS370_Train')
+def view_only_dealer_names():
+    c.execute('SELECT train_no FROM TRAIN')
     data = c.fetchall()
     return data
 
 
-def get_Train(Product_name):
-    c.execute('SELECT * FROM PES2UG20CS370_Train WHERE Name="{}"'.format(Product_name))
+def get_dealer(train_name):
+    c.execute('SELECT * FROM TRAIN WHERE train_name="{}"'.format(train_name))
     data = c.fetchall()
     return data
 
 
-def edit_Train_data(new_Train_no,new_Name,new_Train_type,new_source,new_Destination,new_Availability,Train_no,Name,Train_type,source,Destination,Availability):
-    c.execute("UPDATE PES2UG20CS370_Train SET Train_No=%s,Name=%s,Train_Type=%s,Source=%s,Destination=%s,Availability=%s WHERE Train_No=%s and Name=%s and Train_Type=%s and Source=%s and Destination=%s and Availability=%s", (new_Train_no, new_Name, new_Train_type, new_source, new_Destination, new_Availability, Train_no, Name, Train_type, source, Destination, Availability))
-
+def edit_dealer_data(new_train_no, new_train_name, new_train_type, new_train_source, new_train_destination,new_train_availability,train_no, train_name, train_type, train_source, train_destination,train_availability):
+    c.execute("UPDATE TRAIN SET train_no=%s, train_name=%s, train_type=%s, train_source=%s, train_destination=%s, train_availability=%s WHERE "
+              "train_no=%s and train_name=%s and train_type=%s and train_source=%s and train_destination=%s and train_availability=%s", (new_train_no, new_train_name, new_train_type, new_train_source, new_train_destination,new_train_availability,train_no, train_name, train_type, train_source, train_destination,train_availability))
     mydb.commit()
-    data = c.fetchall()
-    return data
 
 
-def delete_data(product_name):
-    c.execute('DELETE FROM PES2UG20CS370_Train WHERE Name="{}"'.format(Product_name))
+
+
+def delete_data(train_no):
+    c.execute('DELETE FROM TRAIN WHERE train_no="{}"'.format(train_no))
     mydb.commit()
