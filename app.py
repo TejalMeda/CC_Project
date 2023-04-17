4f8b8d11417655b04580612ee1b204d35e2cb8cc
Tenzin Tsephel <108427228+Bakashi160302@users.noreply.github.com>
2023-04-17 19:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from read import read
 from update import update
 
 mydb = mysql.connector.connect(
-    host="cc-project.cv2rz4kihl7u.us-east-1.rds.amazonaws.com",
-    user="admin",
-    password="password123",
-    port = "3306",
-    database="PES2UG20CS370_Train_Lab10"
+    host="cc-project.cvcpbdfon0xp.us-east-1.rds.amazonaws.com",  # Replace with your RDS endpoint
+    port="3306",             # Replace with your RDS port number, typically 3306 for MySQL
+    user="root",           # Replace with your RDS username
+    password="tejal0910&",       # Replace with your RDS password
+    database="PES2UG20CS370_Train_lab10"
 )
 c = mydb.cursor()
 
