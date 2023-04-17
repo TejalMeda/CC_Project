741c927b5ac1e86ab3ca8043c4565bb9b77f580d
Tenzin Tsephel <108427228+Bakashi160302@users.noreply.github.com>
2023-04-17 19:32:20 +0530

    Update create.py

--- create.py
+++ create.py
@@ -5,13 +5,13 @@ from database import add_data
 def create():
     col1, col2 = st.columns(2)
     with col1:
-        Train_no = st.text_input("Number:")
-        Name = st.text_input("Name:")
-        Train_type = st.text_input("Train_type:")
+        train_no = st.text_input("Train No:")
+        train_name = st.text_input("Name:")
     with col2:
-        source = st.selectbox("Source", ["Bangalore", "Chennai", "Mumbai","Managaluru"],key='source')
-        Destination = st.selectbox("Destination", ["Bangalore", "Chennai", "Mumbai","Managaluru"],key='Dest')
-        Availability = st.radio("Availability", ["Yes", "No"])
+        train_type = st.text_input("Train_Type:")
+        train_source = st.text_input("Source:")
+    train_destination = st.text_input("Destination:")
+    train_availability = st.text_input("Availability:")
     if st.button("Add Train"):
-        add_data(Train_no, Name, Train_type, source, Destination, Availability)
-        st.success("Successfully added Train: {}".format(Name))
+        add_data(train_no, train_name, train_type, train_source, train_destination,train_availability)
+        st.success("Successfully added Train: {}".format(train_no))
